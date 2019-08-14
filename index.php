60664b61483d076e0d6013f77770da57f9ddffda
dalt4 <45166154+dalt4@users.noreply.github.com>
2019-08-14 11:48:00 +0300

    Update index.php
    Вместо трех строк 76-78 одна 76 (тернарный оператор)

--- index.php
+++ index.php
@@ -73,9 +73,7 @@ $show_complete_tasks = rand(0, 1);
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
                         <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                            <?php if ($show_complete_tasks): ?>
-                                checked
-                            <?php endif ?>
+                           <?php echo($show_complete_tasks ? 'checked':'') ?>
                         >
 
                         <span class="checkbox__text">Показывать выполненные</span>
