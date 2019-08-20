2c858bc8824077517993e41cecd26f0a7cb4515e
dalt4 <45166154+dalt4@users.noreply.github.com>
2019-08-20 16:53:09 +0300

    зачеркни меня кексобот
    выделение задач, до даты выполнения которых осталось меньше 24 часов.

--- templates/main.php
+++ templates/main.php
@@ -38,9 +38,8 @@
         <label class="checkbox">
             <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
             <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                <?php echo($show_complete_tasks ? 'checked':'') ?>
+                <?= $show_complete_tasks ? 'checked':'' ?>
             >
-
             <span class="checkbox__text">Показывать выполненные</span>
         </label>
     </div>
@@ -48,7 +47,10 @@
     <table class="tasks">
         <?php foreach ($tasks as $value):?>
             <?php if ($value['done'] && !$show_complete_tasks) {continue;} ?>
-            <tr class="tasks__item task <?php echo($value['done'] ? 'task--completed':'') ?>">
+            <tr class="tasks__item task
+                <?= $value['done'] ? 'task--completed ':' ' ?>
+                <?= (strtotime($value['doneDate']) - time())/3600 <= 24 && $value['doneDate'] ? 'task--important':'' ?>
+            ">
                 <td class="task__select">
                     <label class="checkbox task__checkbox">
                         <input class="checkbox__input visually-hidden" type="checkbox" >
