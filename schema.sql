733a142618791aff7a14895fb1f2c6ce000c0b85
dalt4 <45166154+dalt4@users.noreply.github.com>
2019-09-29 18:53:14 +0300

    исправлены ошибки добавлена проверка длины проверка на пустой запрос

--- schema.sql
+++ schema.sql
@@ -7,29 +7,29 @@ USE doingsdone;
 CREATE TABLE IF NOT EXISTS categories
 (
     id      INT(11) AUTO_INCREMENT PRIMARY KEY,
-    name    CHAR(255) NOT NULL,
-    user_id INT(11)   NOT NULL
+    name    CHAR(50) NOT NULL,
+    user_id INT(11)  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tasks
 (
     id            INT(11) AUTO_INCREMENT PRIMARY KEY,
-    add_date      TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    status        TINYINT          NOT NULL DEFAULT 0,
-    name          CHAR(255) UNIQUE NOT NULL,
-    user_file     CHAR(255)                 DEFAULT NULL,
-    done_date     DATE                      DEFAULT NULL,
-    user_id       INT(11)          NOT NULL,
-    categories_id INT(11)          NOT NULL
+    add_date      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status        TINYINT         NOT NULL DEFAULT 0,
+    name          CHAR(50) UNIQUE NOT NULL,
+    user_file     CHAR(255)                DEFAULT NULL,
+    done_date     DATE                     DEFAULT NULL,
+    user_id       INT(11)         NOT NULL,
+    categories_id INT(11)         NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
     id       INT(11) AUTO_INCREMENT PRIMARY KEY,
-    add_date TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    email    CHAR(255)        NOT NULL UNIQUE,
-    name     CHAR(255) UNIQUE NOT NULL,
-    password CHAR(255)        NOT NULL
+    add_date TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email    CHAR(50)        NOT NULL UNIQUE,
+    name     CHAR(50) UNIQUE NOT NULL,
+    password CHAR(100)       NOT NULL
 );
 
 CREATE FULLTEXT INDEX ft ON tasks (name);
